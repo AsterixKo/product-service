2731b53ee3902b8aa11754adfe8cda46f444d9ae
Javier Moneo <jvr.moneo@gmail.com>
2025-05-22 17:12:52 +0200

    add Product and category

---
+++ src/main/java/com/example/product_service/models/entities/Product.java
@@ -0,0 +1,36 @@
+package com.example.product_service.models.entities;
+
+import com.example.product_service.models.enums.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
---
+++ src/main/java/com/example/product_service/models/enums/Category.java
@@ -0,0 +1,5 @@
+package com.example.product_service.models.enums;
+
+public enum Category {
+    AAA, AA, A
+}
