3f1935e4b9e7b7a80f0f5b560982ceb9f59a8bad
Javier Moneo <jvr.moneo@gmail.com>
2025-05-24 17:31:51 +0200

    add productservice

---
+++ src/main/java/com/example/product_service/exceptions/ErrorSavingProductException.java
@@ -0,0 +1,7 @@
+package com.example.product_service.exceptions;
+
+public class ErrorSavingProductException extends Exception {
+  public ErrorSavingProductException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/example/product_service/services/ProductService.java
@@ -0,0 +1,54 @@
+package com.example.product_service.services;
+
+import com.example.product_service.exceptions.ErrorSavingProductException;
+import com.example.product_service.exceptions.ProductNotFoundException;
+import com.example.product_service.models.entities.Product;
+import com.example.product_service.repositories.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product saveProduct(Product product) throws ErrorSavingProductException {
+        try {
+            return productRepository.save(product);
+        } catch (Exception e) {
+            log.error("Error guardando product {}", e.getMessage());
+            throw new ErrorSavingProductException("Error guardando product");
+        }
+    }
+
+    public List<Product> saveListProducts(List<Product> products) throws ErrorSavingProductException {
+        try {
+            return productRepository.saveAll(products);
+        } catch (Exception e) {
+            log.error("Error guardando product {}", e.getMessage());
+            throw new ErrorSavingProductException("Error guardando product");
+        }
+    }
+
+    public Product updateStockProduct(Long idProduct, Integer sale) throws ProductNotFoundException {
+        Optional<Product> product = productRepository.findById(idProduct);
+        if (product.isPresent()) {
+            Integer stock = product.get().getStock();
+            product.get().setStock(stock - sale);
+            return productRepository.save(product.get());
+        } else {
+            log.error("Error product not found");
+            throw new ProductNotFoundException("Error product not found");
+        }
+    }
+}
